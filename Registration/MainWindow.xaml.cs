b959f7e748bcb0a54f36ca1c8f0153f7cce16032
ilyaghrischenko <ilyahryshchenko@gmail.com>
2024-02-21 20:36:43 +0200

    Added files

---
+++ Registration/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Windows;
+
+namespace Registration
+{
+    public partial class MainWindow : Window
+    {
+        private string path = "\\RegisteredUsers.json";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private List<User> LoadRegisteredUsers()
+        {
+            List<User> registeredUsers = new List<User>();
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException();
+            }
+
+            string jsonString = File.ReadAllText(path);
+            if (jsonString == string.Empty) return registeredUsers;
+
+            try
+            {
+                registeredUsers = JsonSerializer.Deserialize<List<User>>(jsonString);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+
+            return registeredUsers;
+        }
+
+        private void LogInEvent(object sender, RoutedEventArgs e)
+        {
+            string login = loginInput.Text;
+            string password = passInput.Password;
+
+            User user = new();
+            try
+            {
+                user.Login = login;
+                user.Password = password;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            MessageBox.Show("You're logged in");
+        }
+
+        private void RegistrationEvent(object sender, RoutedEventArgs e)
+        {
+            string login = loginInput.Text;
+            string password = passInput.Password;
+
+            List<User> registeredUsers = new();
+            try
+            {
+                registeredUsers = LoadRegisteredUsers();
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Error: File not found!");
+                return;
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            try
+            {
+                User user = new(login, password);
+                if (!user.IsValid(registeredUsers)) throw new ArgumentException("Error: This login already exists");
+                registeredUsers.Add(user);
+
+                string jsonString = JsonSerializer.Serialize(registeredUsers);
+                MessageBox.Show(jsonString);
+                File.WriteAllText(path, jsonString);
+
+                MessageBox.Show("You have been successfuly registered!");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Registration/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Registration
+{
+    public class User
+    {
+        private string _login = "NoLogin123";
+        private string _password = "NoPassword123";
+
+        public User() { }
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+
+        [JsonPropertyName("login")]
+        public string Login
+        {
+            get { return _login; }
+            set
+            {
+                if (value == string.Empty) throw new ArgumentException("Error: Login cannot be empty");
+
+                uint kilk_upper = 0;
+                uint kilk_digits = 0;
+                foreach (var item in value)
+                {
+                    if (char.IsDigit(item)) ++kilk_digits;
+                    if (char.IsUpper(item)) ++kilk_upper;
+                }
+
+                if (kilk_upper >= 1 && kilk_digits >= 2) _login = value;
+                else throw new ArgumentException("Error: Login must contain at least one uppercase letter and two digits");
+            }
+        }
+
+        [JsonPropertyName("password")]
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value == string.Empty) throw new ArgumentException("Error: Password cannot be empty");
+
+                uint kilk_upper = 0;
+                uint kilk_digits = 0;
+                foreach (var item in value)
+                {
+                    if (char.IsDigit(item)) ++kilk_digits;
+                    if (char.IsUpper(item)) ++kilk_upper;
+                }
+
+                if (kilk_upper >= 1 && kilk_digits >= 2) _password = value;
+                else throw new ArgumentException("Error: Password must contain at least one uppercase letter and two digits");
+            }
+        }
+
+        public bool IsValid(List<User> users)
+        {
+            foreach (var item in users)
+            {
+                if (item.Login == Login) return false;
+            }
+            return true;
+        }
+    }
+}
